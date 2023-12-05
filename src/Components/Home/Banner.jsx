da619cca7ae8b9ff46b51afa89081333d2b33d06
Sabbir Sarker <masabbir1257@gmail.com>
2023-12-06 02:49:20 +0600

    bannser update and push

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className='items-center justify-between px-3 md:pl-64 md:flex'>
             <div>
-                <p className=''></p>
+                <p className='border-2 border-[#7BC0E3] w-[18%] mb-6'></p>
                 <h1 className='text-6xl font-black mb-7'>We help you create  your <span className='text-[#60E1CB]'>website</span></h1>
                 <p className='text-[#000000]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ut turpis <br /> adipiscing tempus, magna elit nunc iaculis sit. Libero velit quis leo non. <br /> At donec egestas cras in libero pellentesque. Donec amet phasellus</p>
                 <div className='flex gap-4 mt-14'>
