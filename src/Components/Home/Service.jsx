e9c0025c5ddd13ab2e1598611437d7cc411a097d
Sabbir Sarker <masabbir1257@gmail.com>
2023-12-07 00:55:06 +0600

    service card section push

--- src/Components/Home/Service.jsx
+++ src/Components/Home/Service.jsx
@@ -9,13 +9,13 @@ const Service = () => {
             .then(data => setCarts(data))
     }, [])
     return (
-        <div className='text-center pt-28 pb-7 bg-[#E9FBF8] '>
-            <h1 className='mb-6 text-5xl font-black'>Our Services</h1>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod</p>
-            <p className='mb-12'>tempor invidunt ut labore et.</p>
-            <div>
+        <div className=' pt-28 pb-7 bg-[#E9FBF8] '>
+            <h1 className='mb-6 text-5xl font-black text-center'>Our Services</h1>
+            <p className='text-center'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod</p>
+            <p className='mb-12 text-center'>tempor invidunt ut labore et.</p>
+            <div className='grid px-3 md:grid-cols-3 md:pl-64'>
                 {
-                    carts.map((singleCard , i) => <ServiceSingleCard key={i} singleCard={singleCard} />)
+                    carts.map((singleCard , i) => <ServiceSingleCard  key={i} singleCard={singleCard} />)
                 }
             </div>
         </div>
--- src/Components/Home/ServiceSingleCard.jsx
+++ src/Components/Home/ServiceSingleCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { GoArrowRight } from "react-icons/go";
 
-const ServiceSingleCard = ({singleCard}) => {
-    const {id , name} = singleCard;
+const ServiceSingleCard = ({ singleCard }) => {
+    const { id, name, details, img } = singleCard;
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className='px-8 py-11 w-[60%] bg-[#FDFDFF] my-5 border-t-4 rounded border-[#60E1CB]'>
+            <img className='pb-9' src={img} alt="" />
+            <h1 className='pb-4 text-2xl font-semibold'>{name}</h1>
+            <p className='pb-8'>{details}</p>
+            <button><Link className='flex items-center gap-2 text-[#7BC0E3]' to="/">Read More <GoArrowRight />
+            </Link></button>
         </div>
     );
 };
